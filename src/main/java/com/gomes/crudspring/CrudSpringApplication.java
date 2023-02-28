37794871984f27d41eb3e989a40b295a68a795b3
guigomes91 <guilherme.gomes91@outlook.com>
2023-02-27 22:07:54 -0300

    Create controller to list courses with H2

--- src/main/java/com/gomes/crudspring/CrudSpringApplication.java
+++ src/main/java/com/gomes/crudspring/CrudSpringApplication.java
@@ -1,13 +1,29 @@
 package com.gomes.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.gomes.crudspring.model.Course;
+import com.gomes.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository repository) {
+		return args -> {
+			repository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("Tecnologia");
+			repository.save(c);
+		};
 	}
 
 }
---
+++ src/main/java/com/gomes/crudspring/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.gomes.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gomes.crudspring.model.Course;
+import com.gomes.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository repository;
+
+    @GetMapping
+    public List<Course> list() {
+        return repository.findAll();
+    }
+}
