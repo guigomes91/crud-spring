4897f14cab87f2aeebba821b1bb1e7f43eccc54d
guigomes91 <guilherme.gomes91@outlook.com>
2023-03-05 08:00:38 -0300

    Ajuste projeto

--- src/main/java/com/gomes/crudspring/controller/CourseController.java
+++ src/main/java/com/gomes/crudspring/controller/CourseController.java
@@ -2,9 +2,13 @@ package com.gomes.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gomes.crudspring.model.Course;
@@ -22,5 +26,11 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return repository.save(course);
     }
 }
--- src/main/java/com/gomes/crudspring/repository/CourseRepository.java
+++ src/main/java/com/gomes/crudspring/repository/CourseRepository.java
@@ -1,11 +1,9 @@
 package com.gomes.crudspring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.gomes.crudspring.model.Course;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
